a7032976ffbdc7919bb38229a78e1c9d40012c88
Christian R <52263512+ckevinroque@users.noreply.github.com>
2023-03-30 02:48:08 +0800

    Update fasthomepurchase-v2.1.js

--- fasthomepurchase-v2.0.js
+++ fasthomepurchase-v2.0.js
@@ -325,7 +325,7 @@
 
 
       function progress_bar(status){
-        var step = 10;
+        var step = 9.09;
         var current_width = parseFloat($("#progress-status")[0].style.width);
         var step_number = parseFloat($("#current-step-label").text());
         var total_width = 0;
